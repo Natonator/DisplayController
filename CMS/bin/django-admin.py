63720c3aa9c498b2112668a05316153c4ed03268
Nate <nate.a.adams@gmail.com>
2016-04-14 00:46:12 -0600

    Initial commit

---
+++ CMS/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nate/projects/Nadams/Python/CMS-2/CMS/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ cms/displayController/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,"displayController/home.html", {'home': "Welcome home!"})
+    # return render_to_response("displayController/home.html", {'home': "Welcome home!"})
